8abc1499eef01d80d60614ed5692ee30ff4df259
Tarun Tiwari <tiwaritarun700@gmail.com>
2025-01-15 16:05:55 +0530

    connectwallllet

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -2,14 +2,25 @@ import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../config";
 import { Web3Provider } from "@ethersproject/providers";
 import { Contract } from "ethers";
 
-export const getContract = async () =>{
+export const getContract = async () => {
     const { ethereum } = window;
-    if (!ethereum) throw new Error("Ethereum wallet is not connected!");
+    if (!ethereum) throw new Error("Ethereum wallet is not installed!");
+
     const provider = new Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-    if (accounts.length === 0) {
-        alert("No accounts connected. Please connect your wallet.");
+
+    // Request wallet connection
+    try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+        if (accounts.length === 0) {
+            alert("No accounts connected. Please connect your wallet.");
+            return;
+        }
+
+        const signer = provider.getSigner();
+        return new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+    } catch (err) {
+        console.error("Error connecting wallet:", err);
+        throw new Error("Failed to connect wallet");
     }
-    return new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
-}
+};
